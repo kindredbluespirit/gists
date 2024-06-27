7daad5886125f6e2d126596cfb179bb15e1189ff
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-06-27 00:02:11 -0500

    Update cuda-install.sh

--- cuda-install.sh
+++ cuda-install.sh
@@ -10,3 +10,9 @@ nvidia-smi
 ## cuda-samples
 pacman -Qqs gcc
 make HOST_COMPILER=g++-13
+
+## three pieces of software must be compatible with each other versionwise
+## 1. nvidia drivers (from the nvidia package)
+## 2. gcc compiler
+## 3. cuda framework
+## https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
