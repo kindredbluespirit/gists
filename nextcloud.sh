5bb24585a0f357c0da4d331246600a7de28c9d29
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-07-04 14:17:04 -0500

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -4,7 +4,7 @@
 ## https://github.com/nextcloud/all-in-one#nextcloud-all-in-one
 
 # For Linux and without a web server or reverse proxy (like Apache, Nginx, Cloudflare Tunnel and else) already in place:
-sudo docker run \
+docker run \
 --init \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
