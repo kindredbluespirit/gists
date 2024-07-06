4571f642efd2706c203a2ed0669466406bf86a9f
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-07-06 04:33:06 -0500

    Update vscodium-devcontainers.sh

--- vscodium-devcontainers.sh
+++ vscodium-devcontainers.sh
@@ -1,19 +1,25 @@
 ## vscode-remote-ssh
 ssh-copy-id -i .ssh/id_ed25519 user@<hostname>
 
-## vscode-server url being used automatically, wrong commit
-https://update.code.visualstudio.com/commit:c3f126316369cd610563c75b1b1725e0679adfb3/server-linux-x64/stable
+## don't recall where this came from
+# https://update.code.visualstudio.com/commit:c3f126316369cd610563c75b1b1725e0679adfb3/server-linux-x64/stable
 
 ## obtained from vscode -> menu -> help -> about
+## vscodium's version
 ```
 Release: 24171
 Commit: 7d98c6de73fb939f1eb4b8a500c569b858b2d297
 ```
 
-## https://github.com/VSCodium/vscodium/issues/205
-## https://github.com/gitpod-io/openvscode-server
+## devcontainers tried this url, didn't work
+## probably because it's a codium commit and not a vscode one, so it's not available on the official website
+## https://update.code.visualstudio.com/commit:7d98c6de73fb939f1eb4b8a500c569b858b2d297/server-linux-x64/stable
 
 ## https://stackoverflow.com/questions/56073276/how-can-i-install-vs-code-server-manually-and-tell-vs-code-remote
 ## https://stackoverflow.com/questions/56718453/using-remote-ssh-in-vscode-on-a-target-machine-that-only-allows-inbound-ssh-co/56781109#56781109
 ## https://api.github.com/repos/microsoft/vscode/releases
-https://update.code.visualstudio.com/commit:7d98c6de73fb939f1eb4b8a500c569b858b2d297/server-linux-x64/stable
+
+## this could be the answer
+## https://github.com/VSCodium/vscodium/issues/205
+## https://github.com/gitpod-io/openvscode-server
+
