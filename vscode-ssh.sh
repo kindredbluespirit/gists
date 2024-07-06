c01d27f6f29c8b78b9c848664f9f339899d5041d
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-07-06 04:23:46 -0500

    Update and rename vscode-ssh.sh to vscodium-devcontainers.sh

--- vscode-ssh.sh
+++
@@ -1,1 +0,0 @@
-ssh-copy-id -i .ssh/id_ed25519 user@<hostname>
---
+++ vscodium-devcontainers.sh
@@ -0,0 +1,19 @@
+## vscode-remote-ssh
+ssh-copy-id -i .ssh/id_ed25519 user@<hostname>
+
+## vscode-server url being used automatically, wrong commit
+https://update.code.visualstudio.com/commit:c3f126316369cd610563c75b1b1725e0679adfb3/server-linux-x64/stable
+
+## obtained from vscode -> menu -> help -> about
+```
+Release: 24171
+Commit: 7d98c6de73fb939f1eb4b8a500c569b858b2d297
+```
+
+## https://github.com/gitpod-io/openvscode-server
+## https://github.com/gitpod-io/openvscode-server
+
+## https://stackoverflow.com/questions/56073276/how-can-i-install-vs-code-server-manually-and-tell-vs-code-remote
+## https://stackoverflow.com/questions/56718453/using-remote-ssh-in-vscode-on-a-target-machine-that-only-allows-inbound-ssh-co/56781109#56781109
+## https://api.github.com/repos/microsoft/vscode/releases
+https://update.code.visualstudio.com/commit:7d98c6de73fb939f1eb4b8a500c569b858b2d297/server-linux-x64/stable
