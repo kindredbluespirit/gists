60689e5c44eff840adc4a5de0a0de0fdfb2d4076
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-07-08 01:39:35 -0500

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -2,3 +2,15 @@
 ## Run ssh -v to verify the issue
 ## https://superuser.com/questions/187779/too-many-authentication-failures-for-username
 ssh -o PubkeyAuthentication=no username@<ip-address>
+
+## ssh through proxy
+ssh -J <proxy-user>@<proxy-host> user@host
+
+## ssh through proxy (config version)
+## ~/.ssh/config
+```
+Host <any>
+Hostname <destination-ip>
+User <username>
+ProxyJump <proxy-user>@<proxy-host>
+```
