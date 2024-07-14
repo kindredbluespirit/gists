d52649175ea842d9d657fa752b080a7cde246e92
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-07-14 02:57:36 -0500

    Create arch-android.sh

---
+++ arch-android.sh
@@ -0,0 +1,20 @@
+## Android development environment on archlinux
+
+## adb
+pacman -S android-tools 
+
+## sdkmanager cmdline
+yay -S android-sdk-cmdline-tools-latest
+
+## sdk
+yay -S android-platform
+
+#### Flutter ################
+
+## Install flutter, dart extensions on vscode/codium
+
+flutter create <project-directory>
+flutter run
+
+flutter doctor
+flutter devices
