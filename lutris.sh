65ba05793bfab8fe3ab352ce3906515e6eb3f2a5
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-12-30 21:41:30 -0600

    Update lutris.sh

--- lutris.sh
+++ lutris.sh
@@ -2,6 +2,7 @@
 
 ## lutris keeps deleting wineprefix (initially ~/unshared/wineprefix, ~/.wine)
 ## Always add wine game manually
+## https://www.reddit.com/r/wine_gaming/comments/ejoh2x/manually_installing_games_with_lutris/
 ## Install wine through system, not lutris
 pacman -S wine-staging
 
