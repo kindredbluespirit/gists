7a32eef7aab1faa361839b4094bf7448ab1a78d0
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2024-12-30 21:38:13 -0600

    Update lutris.sh

--- lutris.sh
+++ lutris.sh
@@ -1,4 +1,9 @@
 ## Wine
 
+## lutris keeps deleting wineprefix (initially ~/unshared/wineprefix, ~/.wine)
+## Always add wine game manually
+## Install wine through system, not lutris
+pacman -S wine-staging
+
 ## https://github.com/doitsujin/dxvk/issues/3191#issuecomment-1396486397
 pacman -S lib32-vulkan-radeon lib32-vulkan-intel
