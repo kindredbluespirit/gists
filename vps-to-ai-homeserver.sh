95fcc6c738b398613905db6a621975625bc99d76
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2025-01-21 21:51:41 -0600

    Update vps-to-ai-homeserver.sh

--- vps-to-ai-homeserver.sh
+++ vps-to-ai-homeserver.sh
@@ -61,6 +61,7 @@ ollama run llama3
 ####################
 
 ## improved
+## https://github.com/open-webui/open-webui/discussions/4376#discussioncomment-10245922
 
 ## gpu version
 docker run -d -e PORT=3000 --gpus all --network=host -e OLLAMA_BASE_URL=http://127.0.0.1:11434 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
