07d2c66d90ffb6b42d33a8e0466280fcb8dd4e49
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2025-02-02 21:33:44 -0600

    Update oreo-cursors.sh

--- oreo-cursors.sh
+++ oreo-cursors.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/varlesh/oreo-cursors.git
 cd oreo-cursors
 ruby generator/convert.rb
-DBUS_SESSION_BUS_ADDRESS="" make build
+# DBUS_SESSION_BUS_ADDRESS="" make build
+make build
 sudo make install
 
