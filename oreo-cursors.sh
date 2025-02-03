617df9c0b9d4f46f241fddbd845372d38507daa4
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2025-02-02 20:35:07 -0600

    Create oreo-cursors.sh

---
+++ oreo-cursors.sh
@@ -0,0 +1,7 @@
+## https://github.com/varlesh/oreo-cursors
+git clone https://github.com/varlesh/oreo-cursors.git
+cd oreo-cursors
+ruby generator/convert.rb
+DBUS_SESSION_BUS_ADDRESS="" make build
+sudo make install
+
