e222d0c31e60b8bd0f9eb3ed8a0ce6fd277db857
Sai Tarun Inaganti <130604506+kindredbluespirit@users.noreply.github.com>
2025-02-04 02:29:41 -0600

    Create safetensors_info.py

---
+++ safetensors_info.py
@@ -0,0 +1,19 @@
+from safetensors.torch import load_file
+
+## Load safetensors file
+# file_path = "cambrian-8b/model-00001-of-00007.safetensors"
+file_path = "deepseek-r1-1.5b/model.safetensors"
+
+
+state_dict = load_file(file_path)
+
+# Print available keys (usually includes architecture-related names)
+[print(key) for key in state_dict.keys()]
+
+print("---------")
+
+from safetensors import safe_open
+
+with safe_open(file_path, framework="pt") as f:
+    metadata = f.metadata()
+    print(metadata)
